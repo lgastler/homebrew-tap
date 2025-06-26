3ee0d8226747d0ca61a9b72428dfdee43ef07723
Lennart Gastler <lennart@lgastler.de>
2025-06-26 15:29:00 +0200

    fix(calverge): correct class name casing

--- Formula/calverge.rb
+++ Formula/calverge.rb
@@ -1,5 +1,5 @@
 
-class calverge < Formula
+class Calverge < Formula
   homepage "https://github.com/lgastler/calverge-cli"
   url "https://github.com/lgastler/calverge-cli/releases/download/v0.1.2/calverge-0.1.2-macos.tar.gz"
   sha256 "e522a48c48bed406f390df322f48f1f635c1ed9f3e1388903a7cd847e0375761"
